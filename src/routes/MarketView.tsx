9af3c4819ecaa07fae3fe0132b585da6781632d5
Gideon <eignnx@gmail.com>
2024-11-18 15:46:44 -0500

    impl market-inventory side-by-side (initial)

--- src/routes/MarketView.tsx
+++ src/routes/MarketView.tsx
@@ -45,13 +45,91 @@ export default function MarketView() {
     const { playerInventory, market } = useLoaderData() as MarketViewLoaderData;
     const fetcher = useFetcher();
 
+    type OrderedInventories = {
+        common: { [comm in Commodity]: { player: number, market: number; } };
+        playerOnly: { [comm in Commodity]: number };
+        marketOnly: { [comm in Commodity]: number };
+    };
+
+    const orderedInventories: OrderedInventories = {
+        common: {} as any,
+        playerOnly: {} as any,
+        marketOnly: {} as any,
+    };
+
+    for (const commKey in playerInventory) {
+        const comm = commKey as Commodity;
+        const playerQty = playerInventory[comm]!;
+        if (playerQty < 1) { continue; }
+
+        const marketQty = market.inventory[comm];
+        if (marketQty !== undefined && marketQty > 0) {
+            orderedInventories.common[comm] = {
+                player: playerQty,
+                market: marketQty
+            };
+        } else {
+            orderedInventories.playerOnly[comm] = playerQty;
+        }
+
+    }
+
+    for (const commKey in market.inventory) {
+        const comm = commKey as Commodity;
+        const marketQty = market.inventory[comm]!;
+        if (marketQty < 1) { continue; }
+
+        if (playerInventory[comm] === undefined) {
+            orderedInventories.marketOnly[comm] = marketQty;
+        }
+    }
+
     return (<>
         <h1>Market</h1>
         <div className="inventory-market-side-by-side">
+            <div className="player-inventory">
+                <h2>Your Inventory</h2>
+                <div className="inventory-display">
+                    {Object.entries(orderedInventories.common).map(([comm, { player: qty }]) => {
+                        return (
+                            <fetcher.Form method="PATCH">
+                                <div className='inventory-row'>
+                                    <span className='commodity'>{titleCase(comm)}</span>
+                                    <span>{`x ${qty}`}</span>
+                                    <button type="submit">Sell</button>
+                                </div>
+                                <input type="hidden" name="commodityKind" value={comm} />
+                                <input type="hidden" name="_action" value="sell" />
+                            </fetcher.Form>
+                        );
+                    }
+                    )}
+                    {Object.entries(orderedInventories.playerOnly).map(([comm, qty]) => {
+                        return (
+                            <fetcher.Form method="PATCH">
+                                <div className='inventory-row'>
+                                    <span className='commodity'>{titleCase(comm)}</span>
+                                    <span>{`x ${qty}`}</span>
+                                    <button type="submit">Sell</button>
+                                </div>
+                                <input type="hidden" name="commodityKind" value={comm} />
+                                <input type="hidden" name="_action" value="sell" />
+                            </fetcher.Form>
+                        );
+                    })}
+                    {Object.entries(orderedInventories.marketOnly).map(() => (
+                        <div className='inventory-row'>
+                            <span className='commodity'> - </span>
+                            <span> - </span>
+                            <button disabled>Sell</button>
+                        </div>
+                    ))}
+                </div>
+            </div>
             <div className="market-inventory">
                 <h2>Market Inventory</h2>
                 <div className="inventory-display">
-                    {Object.entries(market.inventory).map(([comm, qty]) => {
+                    {Object.entries(orderedInventories.common).map(([comm, { market: qty }]) => {
                         return (
                             <fetcher.Form method="PATCH">
                                 <div className='inventory-row'>
@@ -68,27 +146,30 @@ export default function MarketView() {
                         );
                     }
                     )}
-                </div>
-            </div>
-            <div className="player-inventory">
-                <h2>Your Inventory</h2>
-                <div className="inventory-display">
-                    {Object.entries(playerInventory).map(([comm, qty]) => {
-                        return (
-                            <fetcher.Form method="PATCH">
-                                <div className='inventory-row'>
-                                    <span className='commodity'>{titleCase(comm)}</span>
-                                    <span>{`x ${qty}`}</span>
-                                    <button type="submit">Sell</button>
-                                </div>
-                                <input type="hidden" name="commodityKind" value={comm} />
-                                <input type="hidden" name="_action" value="sell" />
-                            </fetcher.Form>
-                        );
-                    }
-                    )}
+                    {Object.entries(orderedInventories.playerOnly).map(() => (
+                        <div className='inventory-row'>
+                            <span className='commodity'> - </span>
+                            <span> - </span>
+                            <span> - </span>
+                            <button disabled>Buy</button>
+                        </div>
+                    ))}
+                    {Object.entries(orderedInventories.marketOnly).map(([comm, qty]) => (
+                        <fetcher.Form method="PATCH">
+                            <div className='inventory-row'>
+                                <span className='commodity'>{titleCase(comm)}</span>
+                                <span>{`x ${qty}`}</span>
+                                <span>
+                                    {`${marketPrice(market, comm as Commodity)}`}
+                                </span>
+                                <button type="submit">Buy</button>
+                            </div>
+                            <input type="hidden" name="commodityKind" value={comm} />
+                            <input type="hidden" name="_action" value="buy" />
+                        </fetcher.Form>
+                    ))}
                 </div>
             </div>
         </div>
     </>);
-}
+};
