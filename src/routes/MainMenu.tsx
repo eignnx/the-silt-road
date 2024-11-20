17ea348aa8203735f0c69f55d8ea1f5c04d8b25d
Gideon <eignnx@gmail.com>
2024-11-20 18:18:25 -0500

    fix link in main menu

--- src/routes/MainMenu.tsx
+++ src/routes/MainMenu.tsx
@@ -3,6 +3,6 @@ import { Link } from 'react-router-dom';
 export default function MainMenu() {
     return (<>
         <h1>Main Menu</h1>
-        <Link to="/game/inventory">Inventory</Link>
+        <Link to="/game">Enter Game</Link>
     </>);
 }
